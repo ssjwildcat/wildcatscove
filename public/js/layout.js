a5f336f2209dadaaf0733a097f19099adf42ec7e
SSJWildcat <ssjwildcat@gmail.com>
2026-03-10 20:09:10 -0400

    added graphics pages

--- public/js/layout.js
+++ public/js/layout.js
@@ -82,14 +82,24 @@ function headerHTML() {
 
       <aside class="right-sidebar">
 
-      <img width="76px" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ee9e56d-c6ee-41e6-830d-e2954d0a5e2b/dbjawqk-1d2b10f0-ac3a-4b61-abf3-6b1baaea4629.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi8yZWU5ZTU2ZC1jNmVlLTQxZTYtODMwZC1lMjk1NGQwYTVlMmIvZGJqYXdxay0xZDJiMTBmMC1hYzNhLTRiNjEtYWJmMy02YjFiYWFlYTQ2MjkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.6MtVFie6tMWvJ7ZrJhPKaQrW5VTWxyGFzmdEAa_Zzz8">
-      <img width="76px" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ee9e56d-c6ee-41e6-830d-e2954d0a5e2b/dbiyjc2-761bebc1-5cc0-45b3-b300-fd8be320227a.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi8yZWU5ZTU2ZC1jNmVlLTQxZTYtODMwZC1lMjk1NGQwYTVlMmIvZGJpeWpjMi03NjFiZWJjMS01Y2MwLTQ1YjMtYjMwMC1mZDhiZTMyMDIyN2EucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.mRRICl3DuCgxcKNWLhDT8CzmOX81OqTbNYKMwWtyxMQ">
-      <img width="76px" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ee9e56d-c6ee-41e6-830d-e2954d0a5e2b/dbiy42i-40e62323-5733-4d88-9548-6eb25d044e02.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi8yZWU5ZTU2ZC1jNmVlLTQxZTYtODMwZC1lMjk1NGQwYTVlMmIvZGJpeTQyaS00MGU2MjMyMy01NzMzLTRkODgtOTU0OC02ZWIyNWQwNDRlMDIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.shtbTfDxSNbSe3ZpVI1o2VSAzN_tvX7nL5rdDBzAcG8">
-      <img width="76px" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8cb2184f-fe95-4552-aeb0-f3a30b7ab67f/dautmmd-b5ffd710-eb92-45dd-909e-5a36a1cbd580.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi84Y2IyMTg0Zi1mZTk1LTQ1NTItYWViMC1mM2EzMGI3YWI2N2YvZGF1dG1tZC1iNWZmZDcxMC1lYjkyLTQ1ZGQtOTA5ZS01YTM2YTFjYmQ1ODAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.UyM_2JcW8xfUuugYig0DhEier3oeAWdoSCPob6YozdI">
-      <img width="76px" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/62be36f0-067b-44cb-98b1-c386a08072b6/da3x6c1-62b17861-9249-46bc-adb0-c893591f48b9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82MmJlMzZmMC0wNjdiLTQ0Y2ItOThiMS1jMzg2YTA4MDcyYjYvZGEzeDZjMS02MmIxNzg2MS05MjQ5LTQ2YmMtYWRiMC1jODkzNTkxZjQ4YjkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.36IriVLLgkmAKbWAawWmCj56ygFy-u9igq5evN_oofY">
-      <img width="76px" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ee9e56d-c6ee-41e6-830d-e2954d0a5e2b/dbh0j0n-434125d9-7151-47af-a1c9-f688e405c82f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi8yZWU5ZTU2ZC1jNmVlLTQxZTYtODMwZC1lMjk1NGQwYTVlMmIvZGJoMGowbi00MzQxMjVkOS03MTUxLTQ3YWYtYTFjOS1mNjg4ZTQwNWM4MmYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.YFblTarCnTHIeS5kbQRvZUx1O2-bBsra5zXk3DtKQ_4">
-      <center><a href="/graphics/stamps">More Stamps</a></center>
+      <img width="76px" src="/graphics/buttons/asexuals_now.gif">
+      <img width="76px" src="/graphics/buttons/hatems.gif">
+      <img width="76px" src="/graphics/buttons/internetprivacy.gif">
+      <img width="76px" src="/graphics/buttons/thissiteisgay.gif">
+      <center><a href="/graphics/buttons">More Buttons</a></center>
+      <hr>
 
+      <img width="76px" src="/graphics/stamps/hellovaporwave.png">
+      <img width="76px" src="/graphics/stamps/seasidevaporwave.png">
+      <img width="76px" src="/graphics/stamps/fijivaporwave.png">
+      <img width="76px" src="/graphics/stamps/computervaporwave.png">
+      <img width="76px" src="/graphics/stamps/sidewalkvaporwave.png">
+      <img width="76px" src="/graphics/stamps/streetlightvaporwave.png">
+      <center><a href="/graphics/stamps">More Stamps</a></center>
+      <hr>
+
+      <img src="/graphics/blinkies/iheartdragons.gif">
+      <center><a href="/graphics/blinkies">More Blinkies</a></center>
       </aside>
       `;
 }
